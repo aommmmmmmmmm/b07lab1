e87eb2fc8044d29c2fe4d23fb8577527d547460c
unknown <aommmmmmmmmm>
2024-06-02 21:07:58 -0400

    modified Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,34 +1,83 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Polynomial {
-    private double[] coefs;
-    private int deg;
-
-    public  Polynomial(){
-        deg = 0;
-        double [] c = {0};
-        coefs = c;
+    double[] coefs;
+    int[] expos;
+	
+	//methods
+    public Polynomial(){
+        this.coefs = new double[0];
+		this.expos = new int[0];
     }
 
-    public  Polynomial(double[] a){
-        deg = a.length - 1;
+    public Polynomial(double[] a, int[] b){
         coefs = a;
+		expos = b;
     }
 
+//
     public Polynomial add(Polynomial a){
-        int d = Math.max(this.deg, a.deg);
-        double[] c = new double[d + 1];
-        for(int i = 0; i < this.deg; i++){
-            c[i] += this.coefs[i];
+		if(coefs.length == 0) return a;
+		if(a.coefs.length == 0) return this;
+		int alen = a.expos.length;
+		int plen = this.expos.length;
+		int[] tempe = new int[alen + plen];
+		double[] tempc = new double[alen + plen];
+		System.arraycopy(this.expos, 0, tempe, 0, plen);
+		System.arraycopy(this.coefs, 0, tempc, 0, plen);
+        for(int i = 0; i < plen; i++){
+            for(int j = 0; j < alen; j++){
+				if(a.expos[j] == this.expos[i]){
+					tempc[i] += a.coefs[j];
+				}
+			}
         }
-        for(int i = 0; i < a.deg; i++){
-            c[i] += a.coefs[i];
+		int k = 0;
+        int j = 0;
+        for(int i = 0; i < alen; i++){
+            for(j = 0; j < plen; j++){
+				if(a.expos[i] == tempe[j]){
+					break;
+				}
+			}
+			if(a.expos[i] == tempe[j]){
+				continue;
+			}
+			tempe[plen+k] = a.expos[i];
+			tempc[plen+k] = a.coefs[i];
+			k++;
         }
-        return new Polynomial(c);
+		int[] e = new int[plen+k];
+		double[] c = new double[plen+k];
+		System.arraycopy(tempe, 0, e, 0, plen+k);
+		System.arraycopy(tempc, 0, c, 0, k+plen);		
+        return new Polynomial(c, e);
     }
+
+//
+    public Polynomial multiply(Polynomial m){
+		int len1 = m.coefs.length;
+		int len2 = this.coefs.length;
+		Polynomial p = new Polynomial();	
+        for(int i = 0; i < len1; i++){	
+            double[] c = new double[len2];
+            int[] e = new int[len2];		
+            for(int j = 0; j < len2; j++){
+				c[j] = m.coefs[i] * this.coefs[j];
+				e[j] = this.expos[j] + m.expos[i];
+			}   
+			Polynomial a = new Polynomial(c, e);
+			p = a.add(p);
+         
+        }
+		return p;
+	}
 
     public double evaluate(double x){
         double result = 0;
-        for(int i = 0; i < this.deg; i++){
-            result += coefs[i] * Math.pow(x, i);
+        for(int i = 0; i < expos.length; i++){
+            result += coefs[i] * Math.pow(x, expos[i]);
         }
         return result;
     }
@@ -37,5 +86,12 @@ public class Polynomial {
         double result = this.evaluate(x);
         return result == 0;
     }
-        
+	
+	public Polynomial(File file) throws Exception{
+		BufferedReader input = new BufferedReader(new FileReader(file));
+		String line = input.readLine();
+		input.close();
+		String[] s = line.split("");
+		
+	}
 }
